80adfbe9b71a102ee396c688c111e3a72715ef15
axelhagerback <axel.hagerback@gmail.com>
2023-01-30 12:06:16 +0100

    added all content to home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,22 @@
+<script>
+function addContact() {}
+
+function showContacts() {}
+
+function getContact() {}
+</script>
+
 <template>
   <form method="post">
     <input type="text" name="name" placeholder="name" />
     <input type="text" name="phone" placeholder="phone number" />
-    <button type="submit">Add contact</button>
+    <button type="submit" onclick="addContact()">Add contact</button>
+  </form>
+
+  <button onclick="showContacts()">Show contacts</button>
+
+  <form method="get">
+    <input type="number" name="id" placeholder="input ID" />
+    <button onclick="getContact()">Get contact</button>
   </form>
 </template>
