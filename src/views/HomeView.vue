95fbb99fed141be4d6f48457d5e0f8d84d13b56e
axelhagerback <axel.hagerback@gmail.com>
2023-01-30 11:58:49 +0100

    form for adding contact

---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<template>
+  <form method="post">
+    <input type="text" name="name" placeholder="name" />
+    <input type="text" name="phone" placeholder="phone number" />
+    <button type="submit">Add contact</button>
+  </form>
+</template>
