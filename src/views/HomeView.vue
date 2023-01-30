fb853de4681adf4ad7bc886cb1eb07aaa08b5848
isacchr <isac.christiansson@gmail.com>
2023-01-30 14:21:03 +0100

    Everything works

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,56 @@
 <script>
-function addContact() {}
+export default {
+  methods: {
+    addContact() {
+      let name = document.getElementById("name").value;
+      let phone = document.getElementById("phone").value;
 
-function showContacts() {}
+      fetch("/contact", {
+        method: "POST",
+        body: JSON.stringify({ name: name, phone: phone }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      }).then(async (res) => {
+        console.log("added" + (await res.json()));
+      });
+    },
+    showContacts() {
+      fetch("/contact").then(async (res) => {
+        let contacts = (await res.json()).contacts;
+        let contactDiv = document.getElementById("contacts");
+        contactDiv.innerText = "";
+        contacts.forEach((contact) => {
+          contactDiv.innerText += JSON.stringify(contact) + "\n";
+        });
+      });
+    },
+    getContact() {
+      let contactId = document.getElementById("contactId").value;
+      let contactSearch = document.getElementById("contactSearch");
 
-function getContact() {}
+      fetch(`/contact/${contactId}`).then(async (res) => {
+        let contact = await res.json();
+        contactSearch.innerText = JSON.stringify(contact);
+      });
+    },
+  },
+};
 </script>
 
 <template>
   <form method="post">
-    <input type="text" name="name" placeholder="name" />
-    <input type="text" name="phone" placeholder="phone number" />
-    <button type="submit" onclick="addContact()">Add contact</button>
+    <input type="text" id="name" name="name" placeholder="name" />
+    <input type="text" id="phone" name="phone" placeholder="phone number" />
+    <button type="button" v-on:click="addContact()">Add contact</button>
   </form>
 
-  <button onclick="showContacts()">Show contacts</button>
+  <button v-on:click="showContacts()">Show contacts</button>
+  <div id="contacts"></div>
 
   <form method="get">
-    <input type="number" name="id" placeholder="input ID" />
-    <button onclick="getContact()">Get contact</button>
+    <input type="number" id="contactId" name="id" placeholder="input ID" />
+    <button type="button" v-on:click="getContact()">Get contact</button>
   </form>
+  <div id="contactSearch"></div>
 </template>
